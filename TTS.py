7153f18c96338c8961359b18304e1504962e94c2
gravano <gravano@dc.uba.ar>
2015-04-10 11:19:42 -0300

    Version 0.3 del sistema

--- TTS.py
+++ TTS.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright 2015 Universidad de Buenos Aires
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import random, string, os, sys
 
 # Creamos un directorio temporario donde escribir todas las salidas.
-TMP_DIR = os.getcwd() + '/tmp-' + ''.join(random.choice(string.lowercase) for i in range(10))
+TMP_DIR = '/tmp/tmp-' + ''.join(random.choice(string.lowercase) for i in range(10)) + '/'
 os.system('mkdir ' + TMP_DIR)
+print 'El output sera grabado en', TMP_DIR
 
 if len(sys.argv) == 1:
   print
-  print "Front-End de un Sistema de Síntesis del Habla"
+  print "Sistema de Síntesis del Habla"
   print "Departamento de Computación - Facultad de Ciencias Exactas y Naturales - Universidad de Buenos Aires"
   print
   print "Se requiere la URL de la página de Wikipedia a sintetizar: "
   print
-  print "Sintaxis: ./TTS.py URL" 
+  print "Sintaxis: ./TTS.py URL [voz] " 
   print
-  print "Ejemplo: ./TTS.py http://es.wikipedia.org/wiki/Universidad_de_Buenos_Aires"
+  print "Voces: "
+  print "		Festival_UBA   (selección de unidades, base SECYT)"
+  print "		Festival_el_diphone (español neutro)"
+  print "		MaryTTS_UBA1   (selección de unidades, base SECYT)"
+  print "		MaryTTS_UBA2   (HMM, base SECYT)" 
+  print
+  print "Ejemplo: ./TTS.py http://es.wikipedia.org/wiki/Universidad_de_Buenos_Aires Festival_UBA"
   print
   sys.exit(1)
+
+# Leemos la voz elegida. Por defecto, es Festival HMM.
+voz_elegida = "uba_spanish_arg_secyt_cg"
+if len(sys.argv) >= 3:
+  if sys.argv[2] == "Festival_UBA":
+    voz_elegida = "uba_spanish_arg_secyt_cg"
+  elif sys.argv[2] == "Festival_el_diphone":
+    voz_elegida = "el_diphone"
+  elif sys.argv[2] == "MaryTTS_UBA1":
+    voz_elegida = "uba_secyt"
+  elif sys.argv[2] == "MaryTTS_UBA2":
+    voz_elegida = "uba_secyt-hsmm"
 
 # Preparamos la secuencia de comandos a ejecutar.
 cmd = ''
@@ -40,18 +45,25 @@ cmd = ''
 # Traemos la página de Wikipedia (el proxy es si estamos
 # dentro de *.exp.dc.uba.ar)
 cmd += 'export http_proxy="http://proxy.fcen.uba.ar:8080";'
-cmd += 'cd HTML2TXT-0.2/; wget -qO- "%s" | ./HTML2TXT -o %s/HtmlToTxt.txt ; cd ..;' % (sys.argv[1], TMP_DIR)
+cmd += 'cd HTML2TXT-0.3/; wget -qO- "%s" | ./HTML2TXT -o %s/HtmlToTxt.txt ; cd ..;' % (sys.argv[1], TMP_DIR)
 
 # Separa en oraciones.
-cmd += 'cd SeparaOraciones-0.2/; ./SeparaOraciones -i %s/HtmlToTxt.txt -o %s/TextoSeparado.txt ; cd ..;' % (TMP_DIR, TMP_DIR)
+cmd += 'cd SeparaOraciones-0.3/; ./SeparaOraciones -i %s/HtmlToTxt.txt -o %s/TextoSeparado.txt ; cd ..;' % (TMP_DIR, TMP_DIR)
 
 # Traduce palabras extranjeras.
-cmd += 'cd Traductor-0.2/; ./Traductor -i %s/TextoSeparado.txt -o %s/TextoTraducido.txt ; cd ..;' % (TMP_DIR, TMP_DIR)
+cmd += 'cd Traductor-0.3/; ./Traductor -i %s/TextoSeparado.txt -o %s/TextoTraducido.txt ; cd ..;' % (TMP_DIR, TMP_DIR)
 
 # Normaliza el texto.
-cmd += 'cd Normalizador-0.2/; ./Normalizador -i %s/TextoTraducido.txt -o %s/SalidaNormalizador.txt  > /dev/null; cd ..;' % (TMP_DIR, TMP_DIR)
+cmd += 'cd Normalizador-0.3/; ./Normalizador -i %s/TextoTraducido.txt -o %s/SalidaNormalizador.txt  > /dev/null; cd ..;' % (TMP_DIR, TMP_DIR)
 
-print 'Salida: %s/SalidaNormalizador.txt' % TMP_DIR
+# Para Festival hace falta hacer algunos cambios. Ejemplos: "á"-->"'a", "fin."-->"fin ."
+if voz_elegida=="uba_spanish_arg_secyt_cg" or voz_elegida=="el_diphone":
+  cmd += 'cd BackEndTransf-0.3/; ./BackEndTransf -i %s/SalidaNormalizador.txt -o %s/SalidaBackEndTransf.txt ; cd ..;' % (TMP_DIR, TMP_DIR)
+else:
+  cmd += 'cp %s/SalidaNormalizador.txt %s/SalidaBackEndTransf.txt;' % (TMP_DIR, TMP_DIR)
+
+# Síntesis propiamente dicha, con Festival o Mary TTS.
+cmd += 'cd BackEnd-0.3/; ./BackEnd -i %s/SalidaBackEndTransf.txt -o %s -v %s; cd ..;' % (TMP_DIR, TMP_DIR, voz_elegida)
 
 # Ejecutamos los comandos.
 os.system(cmd)
